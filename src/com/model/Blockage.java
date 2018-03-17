3ab72907a091f17dd421e3bdf60d66ec0068f82f
Danus <kpethodaniel@gmail.com>
2018-03-17 17:58:37 +0100

    Blockage javadoc kommentek

--- src/com/model/Blockage.java
+++ src/com/model/Blockage.java
@@ -1,8 +1,18 @@
 package com.model;
 
+/**
+ * Akadályt képező elemet reprezentál
+ * Ezek az akadályok nem mozdíthatóak el mezőjükről
+ */
 public abstract class Blockage extends Element{
     private boolean stucked;
 
+    /**
+     * Játékos szeretné tolni a továbbhaladás irányába az akadályt
+     * @param player játékos, ami az akadály mezőjére kíván lépni
+     * @param direction a továbbhaladás iránya
+     * @return mindig false-al tér vissza, mert Blockage elem nem mozgatható
+     */
     @Override
     public boolean hit(Player player, Direction direction) {
         System.out.println("-->[Wall :w].hit(worker,direction)");
@@ -10,6 +20,12 @@ public abstract class Blockage extends Element{
         return false;
     }
 
+    /**
+     *
+     * @param pushable tolható objektum, ami az akadály mezőjére kíván lépni
+     * @param direction a továbbhaladás iránya
+     * @return mindig false-al tér vissza, mert Blockage elem nem mozgatható
+     */
     public boolean hit(Pushable pushable, Direction direction) {
         System.out.println("-->[Wall :w].hit(worker,direction)");
         System.out.println("<--false == Nem tudsz ide Lepni");
