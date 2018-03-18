1f8a64dd52b0bc668600e70fcd79f42240557d86
emzeperx <14148936+emzeperix@users.noreply.github.com>
2018-03-18 23:59:03 +0100

    Update Worker.java

--- src/com/model/Worker.java
+++ src/com/model/Worker.java
@@ -6,23 +6,21 @@ package com.model;
 public class Worker extends Player {
 
     /**
-     *
-     * @param direction Egy paraméterül átvett irány,amelyikbe lépni kíván a Worker.
      * A függvény felelős a Worker megfelelő irányba történő léptetéséért.
      * Megnézi, hogy melyik a követekző mező és azon áll e valamilyen element és ennek függvényében
      * hajtja végre a mozgást.
+     * @param direction Egy paraméterül átvett irány,amelyikbe lépni kíván a Worker.
+     *
      */
     @Override
     public void move(Direction direction) {
 
         super.move(direction);
-        //ez hivodik meg , itt hivom meg a metodusokat nezzetek a szekvenciakat , az fog segiteni.
 
         Field nextfield =  getField().getNeighbors(direction);
         Element nextElement = nextfield.getElement();
-
-        //Ez se fog majd kelleni de most KELL
         nextfield.setElement(nextElement);
+        
         if(nextElement != null) {
             nextElement.setTestField(nextfield);
         }
